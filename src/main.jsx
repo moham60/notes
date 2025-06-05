ffca5c9bdb68fe4ac1658d0ffc03152342e9849c
moham60 <alkingg289@gmail.com>
2025-06-05 08:07:45 +0300

    updatedCommit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "flowbite";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import flowbitePlugin from "flowbite/plugin";
+
 
 import tailwindcss from "@tailwindcss/vite";
-import { flowbiteReact } from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react(), flowbitePlugin, flowbiteReact()],
-  base: "/noteApp/",
+  plugins: [tailwindcss(), react()],
+  base: `/noteApp/`,
 });
